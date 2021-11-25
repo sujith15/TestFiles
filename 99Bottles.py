2f4d2b5a034ac0cdafded15f79d5a01b75f16b6d
sujith15 <69566813+sujith15@users.noreply.github.com>
2021-11-24 19:30:24 -0600

    Add files via upload

---
+++ 99Bottles.py
@@ -0,0 +1,33 @@
+def firstVerse(count, bottleTense):
+    firstVerse = "%s %s of beer on the wall, %s %s of beer." % (count, bottleTense, count, bottleTense)
+    print(firstVerse)
+
+def secondVerse(count, bottleTense):
+    secondVerse = "Take one down and pass it around, %s %s of beer on the wall!\n" % (count, bottleTense)
+    print(secondVerse)
+
+def lyrics():
+    count = 99
+    bottleTense = "bottles"
+
+    while count > 1:
+        firstVerse(count, bottleTense)
+        count -= 1
+
+        if count > 1:
+            secondVerse(count, bottleTense)
+        elif count == 1:
+            bottleTense = "bottle"
+            secondVerse(count, bottleTense)
+
+    firstVerse(count, bottleTense)
+
+    count = "no more"
+    bottleTense = "bottles"
+    secondVerse(count, bottleTense)
+
+    count = "No more"
+    firstVerse(count, bottleTense)
+    print("Go to the store and buy some more, 99 bottles of beer on the wall!\n")
+
+lyrics()
---
+++ magic8Ball.py
@@ -0,0 +1,26 @@
+from random import randint
+
+responses = ["Yes", "No", "Probably", "Maybe", "I don't think so", "Perhaps",
+             "It's possible", "Yes, I think so"]
+
+print("Welcome to the Magic 8 ball!")
+
+while True:
+    question = input("Ask me a question: ")
+    print responses[randint(0, 7)]
+
+    while True:
+        play = input("Would you like to play again? (Y/N) ")
+        print ""
+
+        if play == "Y" or play == "y":
+            continue
+
+        elif play == "N" or play == "n":
+            print("Goodbye!")
+            break
+
+        else:
+            print("I didn't quite catch that.")
+
+    break
